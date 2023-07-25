82c334065bc1594865a28279aa60671d317a8553
Putra Dinata <putradnata15@gmail.com>
2023-07-25 22:24:14 +0800

    feat: airports CRUD
    - Add: Airports Create Page (BE integrated)
    - Add: Airports Delete action
    - Add: Airports Update Page (BE Integrated)
    - Add: Car Class Update Page (BE Integrated)
    - Fix: Wrong place of variable

--- src/shared/const/Airport.const.ts
+++ src/shared/const/Airport.const.ts
@@ -1,5 +1,14 @@
-export const initialValues = {
+import * as Yup from 'yup';
+import { AirportType } from '@/src/types/Airport.type';
+
+export const initialValues: AirportType = {
     name: '',
     latitude: '',
     longitude: '',
 };
+
+export const addAirportSchema = Yup.object().shape({
+    name: Yup.string().required('Airport is Required'),
+    latitude: Yup.number().typeError('Latitude must be a Number').required('Latitude is Required'),
+    longitude: Yup.number().typeError('Longitude must be a Number').required('Longitude is Required'),
+});
