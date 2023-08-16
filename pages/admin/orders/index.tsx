d0662f1694b532f4925395c807aa6c2de821a081
Richie Permana <richie.permana@gmail.com>
2023-08-17 02:59:54 +0800

    feat: Integration Orders Page with APIs

--- pages/admin/orders/index.tsx
+++ pages/admin/orders/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setPageTitle } from '@/store/themeConfigSlice';
 import Breadcrumb from '@/components/Layouts/Breadcrumb';
 import { DataTable } from 'mantine-datatable';
@@ -7,11 +7,19 @@ import { Group } from '@mantine/core';
 import { EyeIcon } from '@/src/assets/icons/Eye.icon';
 import Link from 'next/link';
 import Orders from '@/src/data/Orders.json';
+import { AppDispatch, IRootState } from '@/store';
+import { fetchOrdersList } from '@/store/orderSlice';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import { Order } from '@/src/types/entities';
+import { C } from '@fullcalendar/core/internal-common';
 
 export default function OrderIndex() {
     const PAGE_SIZE = 10;
 
-    const dispatch = useDispatch();
+    const dispatch = useDispatch<AppDispatch>();
+    const MySwal = withReactContent(Swal);
+    const { orders, meta } = useSelector((state: IRootState) => state.order);
 
     const [page, setPage] = useState(1);
     const [records, setRecords] = useState(Orders.slice(0, PAGE_SIZE));
@@ -24,7 +32,39 @@ export default function OrderIndex() {
 
     useEffect(() => {
         dispatch(setPageTitle('Orders Data'));
-    });
+
+        const fetchOrders = async () => {
+            const params = {
+                limit: PAGE_SIZE,
+                page,
+                sort: 'ASC',
+                // search: search,
+            };
+
+            try {
+                const response = await dispatch(fetchOrdersList(params)).unwrap();
+
+                console.log({ response });
+            } catch (error: any) {
+                console.log(error);
+
+                MySwal.fire({
+                    title: error.message!,
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 3000,
+                    showCloseButton: true,
+                    customClass: {
+                        popup: `color-danger`,
+                    },
+                });
+            }
+        };
+        fetchOrders();
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [dispatch, page]);
 
     return (
         <>
@@ -46,15 +86,46 @@ export default function OrderIndex() {
                                         {
                                             accessor: 'payment_status',
                                             title: 'Payment Status',
-                                            render: ({ payment_status }) => {
-                                                switch (payment_status) {
-                                                    case 'complete':
-                                                        return <span className="badge bg-success">Complete</span>;
-                                                        break;
+                                            render: ({ payment_status }: Order) => {
+                                                switch (payment_status.name) {
+                                                    case 'Completed':
+                                                        return <span className="badge bg-success">{payment_status.name}</span>;
 
-                                                    case 'pending':
-                                                        return <span className="badge bg-warning">Pending Payment</span>;
-                                                        break;
+                                                    case 'Pending':
+                                                        return <span className="badge bg-pending">{payment_status.name}</span>;
+
+                                                    case 'Refunded':
+                                                        return <span className="badge bg-refunded">{payment_status.name}</span>;
+
+                                                    case 'Denied':
+                                                        return <span className="badge bg-failed">{payment_status.name}</span>;
+                                                }
+                                            },
+                                        },
+                                        {
+                                            accessor: 'order_status',
+                                            title: 'Order Status',
+                                            render: ({ order_status }: Order) => {
+                                                switch (order_status.name) {
+                                                    case 'Completed':
+                                                    case 'Payment Completed':
+                                                        return <span className="badge bg-success">{order_status.name}</span>;
+
+                                                    case 'Cancelled':
+                                                        return <span className="badge bg-cancelled">{order_status.name}</span>;
+
+                                                    case 'In Progress':
+                                                    case 'Refunded':
+                                                    case 'Payment Refunded':
+                                                    case 'Booking Confirmed':
+                                                        return <span className="badge bg-refunded">{order_status.name}</span>;
+
+                                                    case 'Payment Pending':
+                                                        return <span className="badge bg-pending">{order_status.name}</span>;
+
+                                                    case 'Expired':
+                                                    case 'Payment Denied':
+                                                        return <span className="badge bg-failed">{order_status.name} Pending</span>;
                                                 }
                                             },
                                         },
@@ -71,8 +142,8 @@ export default function OrderIndex() {
                                             ),
                                         },
                                     ]}
-                                    records={records}
-                                    totalRecords={Orders.length}
+                                    records={orders}
+                                    totalRecords={meta.totalItems}
                                     recordsPerPage={PAGE_SIZE}
                                     page={page}
                                     onPageChange={(p) => setPage(p)}
