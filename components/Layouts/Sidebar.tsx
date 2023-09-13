a37fea54602c0db7a7723bec60718ef2b5587e2e
Renrensan <pasha.renaisan@gmail.com>
2023-09-13 10:31:10 +0800

    chores(change-sidebar-logo): Change sidebar logo to qicco

--- components/Layouts/Sidebar.tsx
+++ components/Layouts/Sidebar.tsx
@@ -132,7 +132,7 @@ const Sidebar = () => {
                 <div className="h-full bg-white dark:bg-black">
                     <div className="flex items-center justify-between px-4 py-3">
                         <Link href="/" className="main-logo flex shrink-0 items-center">
-                            <img className="ml-[5px] w-36 flex-none" src="/assets/images/logo/limosia-logo-long.png" alt="logo" />
+                            <img className="ml-[5px] w-36 flex-none" src="/assets/images/logo/qicco-logo-long.png" alt="logo" />
                         </Link>
 
                         <button
